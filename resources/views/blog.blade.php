85a353948db5def8f3b8a3da3cad882f1f4ad2a6
yugosetiawanesia <ssyugoslavia@gmail.com>
2024-10-09 13:37:59 +0800

    with-tailwind

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+<h1>Halaman Blog</h1>
+<article>
+    <h3>Judul 1</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, molestias autem. Optio obcaecati laudantium delectus vel at accusantium suscipit, repellendus doloribus reprehenderit numquam voluptas voluptate nulla omnis enim, nobis non?</p>
+</article>
+<article>
+    <h3>Judul 2</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, molestias autem. Optio obcaecati laudantium delectus vel at accusantium suscipit, repellendus doloribus reprehenderit numquam voluptas voluptate nulla omnis enim, nobis non?</p>
+</article>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontak</title>
+</head>
+<body>
+    <h3>Halaman Kontak</h3>
+    <ul>
+    <li>
+        <a href="https://instagram.com">Instagram</a>
+    </li>
+    <li>
+        <a href="https://x.com">X</a>
+    </li>
+    <li>
+        <a href="https://youtube.com">Youtube</a>
+    </li>
+    </ul>
+</body>
+</html>
