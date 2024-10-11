059554e4d7b56c01e1246063ba4ab95e7d83c434
yugosetiawanesia <ssyugoslavia@gmail.com>
2024-10-11 11:33:05 +0800

    View data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl" >Ini Lampiran Blogs</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,54 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Yugoslavia', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Yugoslavia',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Est odit laudantium earum cumque pariatur facilis vitae nulla esse quasi
+            accusantium! Est iste facilis magnam dolor soluta, maxime quo architecto earum!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Yugoslavia',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore
+            qui perferendis tenetur laborum cum explicabo maxime impedit, aliquam possimus.'
+        ],
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Yugoslavia',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Est odit laudantium earum cumque pariatur facilis vitae nulla esse quasi
+            accusantium! Est iste facilis magnam dolor soluta, maxime quo architecto earum!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'slug' => 'judul-artikel-2',
+            'author' => 'Yugoslavia',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore
+            qui perferendis tenetur laborum cum explicabo maxime impedit, aliquam possimus.'
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+// dd($post);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
